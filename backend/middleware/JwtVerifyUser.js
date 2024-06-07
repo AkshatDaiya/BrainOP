abaca5fb694c2fb5ff4efbc6e823133340d4441e
Akshat <daiyaakshat4@gmail.com>
2024-06-07 16:27:54 +0530

    auth added

--- backend/middleware/JwtVerifyUser.js
+++ backend/middleware/JwtVerifyUser.js
@@ -2,12 +2,16 @@ const jwt = require("jsonwebtoken");
 
 const verifyUser = (req, res, next) => {
   console.log(req.cookies);
-  const { jwtoken } = req.cookies;
+  const jwtoken = req.cookies.jwtoken;
+
   if (!jwtoken) {
-    return res.status(400).json("The token is not available");
+    return res.status(400).json({ message: "The token is not available" });
   } else {
     jwt.verify(jwtoken, process.env.JWT_SECRET, (error, decoded) => {
-      if (error) return res.status(401).json("Token is invalid");
+      if (error) {
+        return res.status(401).json({ message: "Token is invalid" });
+      }
+      req.user = decoded;
       next();
     });
   }
