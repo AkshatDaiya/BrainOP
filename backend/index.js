2adcd88fe7403f40a49a6229d6cd191bd4df519d
Akshat <daiyaakshat4@gmail.com>
2024-06-07 15:47:00 +0530

    improve cors setting

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const mongoose = require("mongoose");
-mongoose.connect(`${process.env.DB_URL}`);
-const Router = require("./router/user.router.js");
-const cookieParser = require("cookie-parser");
 
-app.use(cors({
-  origin: 'https://brain-op-front.vercel.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.get('/', (req, res) => {
-  res.send('Server is started...');
+mongoose.connect(`${process.env.DB_URL}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const Router = require("./router/user.router.js");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "https://brain-op-front.vercel.app",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Server is started...");
+});
+
 app.use("/api", Router);
 app.use(express.static("public"));
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running on port no. ${process.env.PORT}`)
-);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port no. ${process.env.PORT}`);
+});
